21bb1e81837cf6f5b9fd5f49b9c1b40b865b1d76
Alexandr Moroz <alexandrmoroz@gmail.com>
2021-10-03 19:04:02 +0300

    настройки проекта. Подготовка к лечению запуска из xcode

---
+++ android/app/src/main/kotlin/ru/aeonika/amonitor/MainActivity.kt
@@ -0,0 +1,6 @@
+package ru.aeonika.amonitor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- android/app/src/main/kotlin/ru/w_cafe/amonitor/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package ru.w_cafe.amonitor
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity() {
-}
