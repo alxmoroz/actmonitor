033cb81f839f5ab96905461693d6ae14110a51c3
Alexandr Moroz <alexandrmoroz@gmail.com>
2021-10-05 03:09:31 +0300

    #1870 доработал в соотв. с документацией к плагину

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,9 +1,14 @@
 import UIKit
 import WidgetKit
 import Flutter
+import workmanager
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+  static func registerPlugins(with registry: FlutterPluginRegistry) {
+    GeneratedPluginRegistrant.register(with: registry)
+  }
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
@@ -58,7 +63,12 @@ import Flutter
 
     UIApplication.shared.setMinimumBackgroundFetchInterval(TimeInterval(60*15))
 
-    GeneratedPluginRegistrant.register(with: self)
+    AppDelegate.registerPlugins(with: self)
+    
+    WorkmanagerPlugin.setPluginRegistrantCallback { registry in
+      AppDelegate.registerPlugins(with: registry)
+    }
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
